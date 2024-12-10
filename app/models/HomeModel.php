d86ce4d68aa317d4fa07a915517535cbb3a5d386
Maurici Dmarco <129069676+MauriciDmarc@users.noreply.github.com>
2024-12-10 19:59:01 -0300

    Create HomeModel.php

---
+++ app/models/HomeModel.php
@@ -0,0 +1,9 @@
+<?php
+
+class HomeModel {
+
+    public function getData() {
+        // Aqui você pode conectar ao banco de dados ou retornar dados estáticos
+        return ['message' => 'Bem-vindo ao sistema MVC!'];
+    }
+}
