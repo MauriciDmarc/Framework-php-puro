56e72c079312d7f399946d89781e558f97a2be73
Maurici Dmarco <129069676+MauriciDmarc@users.noreply.github.com>
2024-12-10 20:00:08 -0300

    Create autoload.php

---
+++ app/core/autoload.php
@@ -0,0 +1,22 @@
+<?php
+
+function autoload($className) {
+    // Define os diretórios onde as classes podem estar
+    $directories = [
+        '../app/controllers/',  // Controllers
+        '../app/models/',       // Models
+        '../core/',             // Core
+    ];
+
+    foreach ($directories as $directory) {
+        // Verifica se o arquivo existe no diretório
+        $file = $directory . $className . '.php';
+        if (file_exists($file)) {
+            require_once $file;
+            return;
+        }
+    }
+}
+
+// Registra o autoload
+spl_autoload_register('autoload');
