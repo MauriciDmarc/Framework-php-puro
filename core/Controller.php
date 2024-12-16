16da56e392d188708668f58ab1ae6150bda05be7
Maurici Dmarco <129069676+MauriciDmarc@users.noreply.github.com>
2024-12-16 20:47:22 -0300

    Atualizar o Controller.php
    comentários

--- core/Controller.php
+++ core/Controller.php
@@ -1,9 +1,14 @@
 <php 
 
 class Controller {
+
+    //Método para renderizar a view
     public function render($view, $data = []){
         
+        //Extrai dados para variáveis 
         extract($data);
+
+        //inclui a view
         require_once "../app/views/{$view}.PHP";
     }
 }
