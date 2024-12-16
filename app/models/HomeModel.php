985a5607c6751448164cf67dbb676e9aec11d794
Maurici Dmarco <129069676+MauriciDmarc@users.noreply.github.com>
2024-12-16 20:18:38 -0300

    Atualizar o HomeModel.php
    Adicionado: conexão com banco

--- app/models/HomeModel.php
+++ app/models/HomeModel.php
@@ -2,8 +2,16 @@
 
 class HomeModel {
 
+    private $db;
+    private $conn;
+
+    public __construct(){
+        $this->db   = new Database();
+        $this->conn = $this->getConn();
+    }
+
     public function getData() {
         // Aqui você pode conectar ao banco de dados ou retornar dados estáticos
-        return ['message' => 'Bem-vindo ao sistema MVC!'];
+        return ['message' => 'Sucesso'];
     }
 }
