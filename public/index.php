9bf2751b3336e6cf75abaa1a79132846fa0d1331
Maurici Dmarco <129069676+MauriciDmarc@users.noreply.github.com>
2025-04-14 08:57:36 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,28 +1,48 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FRAMEWORK MVC PHP</title>
+</head>
+<body class="bg-gray-300">
+    <?php
+    // Inclui o autoloader
+    require_once '../core/autoload.php';
+    date_default_timezone_set('America/Sao_Paulo');
 
-// Inclui o autoloader
-require_once '../core/autoload.php';
+    session_start();
 
-// Cria uma instância do roteador
-$router = new Router();
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
 
-// Adiciona a rota para a home
-$router->addRoute('home', function() {
-    $controller = new HomeController();
-    $controller->index();
-});
+    // Cria uma instância do roteador
+    $router = new Router();
 
-// Adiciona a rota para a rota 'user' com parâmetros
-$router->addRoute('user', function($params) {
-    $controller = new UserController();
-    $controller->show($params);
-});
+    // Adiciona a rota para a agregado
+    $router->addRoute('agregado', function() {
+        $controller = new AgregadoController();
+        $controller->index();
+    });
 
-// Obtém o parâmetro da URL (ex: ?url=home)
-$route = isset($_GET['url']) ? $_GET['url'] : 'home'; // Rota padrão é 'home'
+    // Adiciona a rota para a rota 'user' login
+    $router->addRoute('user/login', function() {
+        $controller = new UserController();
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $controller->login(); // Processa o login (POST)
+        } else {
+            $controller->showLoginForm(); // Exibe o formulário (GET)
+        }
+    });
 
-// Recupera parâmetros adicionais (se existirem)
-$params = array_diff_key($_GET, array('url' => '')); // Remove o 'url' da lista de parâmetros
+    // Obtém o parâmetro da URL (ex: ?url=home)
+    $route = isset($_GET['url']) ? $_GET['url'] : 'agregado'; // Rota padrão é 'user/login'
 
-// Chama o roteador para processar a rota
-$router->dispatch($route, $params);
+    // Recupera parâmetros adicionais (se existirem)
+    $params = array_diff_key($_GET, array('url' => '')); // Remove o 'url' da lista de parâmetros
+
+    // Chama o roteador para processar a rota
+    $router->dispatch($route, $params);
+    ?>
+</body>
+</html>
